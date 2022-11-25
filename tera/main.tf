e18c633f0de7eafd27a73ae793a81dd55b423128
codemidev <ochaijay81@gmail.com>
2022-11-25 01:38:04 +0100

    #Devops

--- tera/main.tf
+++ tera/main.tf
@@ -14,8 +14,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "#ubuntu instance"
   instance_type = "t2.micro"
+  key_name ="#keynane"
 
   tags = {
     Name = "ExampleAppServerInstance"
@@ -24,3 +25,32 @@ resource "aws_instance" "app_server" {
 
 
 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "#windowsfor"
+  instance_type = "t2.micro"
+  key_name ="#keynane"
+
+  tags = {
+    Name = "Ocent"
+  }
+}
+
+~
+~
+~
