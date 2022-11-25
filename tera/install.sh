d0b0321c900b8366e23e12053687fe7ff2b76add
codemidev <ochaijay81@gmail.com>
2022-11-25 03:05:30 +0100

    ShellSpinner

---
+++ tera/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+ls 
+pwd 
+ps
+pkg install figlet
+figlet Devops interview
+echo -n -e "runing req install deamon\n"
+echo -n -e "packages installed\n"
+echo -n -e "Strating Tera ec2 instances\n"
+ls
+ps
+
